65c57ec6e8e41ca8c6f3a982b74f2479785a374b
Andrew <andrewcsnead@gmail.com>
2018-01-28 16:42:27 +0000

    basic tests tddlymi

---
+++ tic.py
@@ -0,0 +1,28 @@
+def test_legal_move_is_legal():
+    move = 0
+    assert is_legal(move, []) is True
+
+def test_low_illegal_move_is_not_legal():
+    move = -1
+    assert is_legal(move, []) is False
+
+def test_high_illegal_move_is_not_legal():
+    move = 9
+    assert is_legal(move, []) is False
+
+def test_another_legal_move_is_legal():
+    move = 1
+    assert is_legal(move, []) is True
+
+def test_move_is_illegal_if_already_played():
+    moves_played = [0]
+    move = 0
+    assert is_legal(move, moves_played) is False
+
+def test_moves_for_player_one_are_at_even_indices():
+    moves_played = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    player_one_moves = []
+    assert player_one_moves == [0, 2, 4, 6, 8]
+
+def is_legal(move, moves_played):
+   return move > -1 and move < 9 and move not in moves_played
