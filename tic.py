7e0c5c659b14ddbf6903dedb7235d1c3780506a6
Andrew <andrewcsnead@gmail.com>
2018-01-29 10:46:51 +0000

    add winning check for all combos

--- tic.py
+++ tic.py
@@ -1,3 +1,5 @@
+import itertools
+
 def test_legal_move_is_legal():
     move = 0
     assert is_legal(move, []) is True
@@ -37,6 +39,10 @@ def test_player_moves_in_a_different_order_win():
     player_moves = [2, 1, 0]
     assert is_win(player_moves) is True
 
+def test_more_than_three_player_moves_still_wins():
+    player_moves = [8, 2, 1, 0]
+    assert is_win(player_moves)
+
 def is_legal(move, moves_played):
    return move > -1 and move < 9 and move not in moves_played
 
@@ -45,9 +51,10 @@ def moves_for_player(player, moves_played):
     return moves_played[player::number_of_players]
 
 def is_win(player_moves):
+    combos = itertools.combinations(player_moves, 3)
     winning_moves = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ]
-    return sorted(player_moves) in winning_moves
+    return any([sorted(combo) in winning_moves for combo in combos])
